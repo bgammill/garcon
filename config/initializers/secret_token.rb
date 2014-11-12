ab9cbd5cbe7f40ed68902c14e5d6d990367ef52b
Bobby Gammill <bgammill@gmail.com>
2014-11-12 18:26:42 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Garcon::Application.config.secret_key_base = 'b691e4eed45e65c8d86502487bc67942dedd94ca9c09fea426b2f9fa9ea73bc35a7fc58de662a1e45d0d2e1e9b7a63c8e8b5fcccbf5e650cb39ff35e4d46a315'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Garcon::Application.config.session_store :cookie_store, key: '_garcon_session'
