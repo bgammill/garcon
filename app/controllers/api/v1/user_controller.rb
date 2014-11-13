533ca732cc4a1c0500f501ef9a36969a8542fe78
Bobby Gammill <bgammill@gmail.com>
2014-11-12 21:22:36 -0500

    Add updated model and api code

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -2,7 +2,7 @@ module Api
     module V1
         class UserController < ApplicationController
             def index
-                render json: {message: 'Resource not found'}, status: 200
+                render json: User.all, status: 200
             end
         end
     end
